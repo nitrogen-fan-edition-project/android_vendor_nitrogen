96c7df48fe572153b94f4248855bd6dcc08dd528
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:49:57 -0300

    vendor: Add permissions for lineagehw
    Change-Id: I55172ac4e9121aa969b628b450781e58151de18c
    
    Conflicts:
    	config/common.mk

--- products/common.mk
+++ products/common.mk
@@ -121,7 +121,8 @@ PRODUCT_COPY_FILES += \
 
 # Priv-app config
 PRODUCT_COPY_FILES += \
-    vendor/nitrogen/config/permissions/privapp-permissions-nitrogen.xml:system/etc/permissions/privapp-permissions-nitrogen.xml
+    vendor/nitrogen/config/permissions/privapp-permissions-nitrogen.xml:system/etc/permissions/privapp-permissions-nitrogen.xml \
+    vendor/nitrogen/prebuilt/common/etc/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineagehw.xml
 
 # Themes
 PRODUCT_PACKAGES += \
