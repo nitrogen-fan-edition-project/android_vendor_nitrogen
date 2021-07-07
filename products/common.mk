f6069e83758a3292cd07de15342373421cb859b0
xyyx <xyyx@mail.ru>
2021-07-07 14:35:59 +0800

    Override default alarm/notification/ringtone sounds

--- products/common.mk
+++ products/common.mk
@@ -136,6 +136,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     StitchImage
 
+# Default notification/alarm/ringtone sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg \
+    ro.config.ringtone=Aquila.ogg
+
 # Boot animations
 $(call inherit-product-if-exists, vendor/nitrogen/products/bootanimation.mk)
 
